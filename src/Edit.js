865845f5cfef1586f1d93a5e3777d80513ce11de
johotpocket <jo.hotpocket@gmail.com>
2017-06-13 14:51:16 -0600

    more edit changes

--- src/Edit.js
+++ src/Edit.js
@@ -9,48 +9,21 @@ const Edit = () => (
     <TextField
       hintText="Hint Text"
     /><br />
-    <br />
     <TextField
-      hintText="The hint text can be as long as you want, it will wrap."
-    /><br />
-    <TextField
-      id="text-field-default"
-      defaultValue="Default Value"
+      hintText="Hint Text"
     /><br />
     <TextField
       hintText="Hint Text"
-      floatingLabelText="Floating Label Text"
-    /><br />
-    <TextField
-      defaultValue="Default Value"
-      floatingLabelText="Floating Label Text"
     /><br />
     <TextField
       hintText="Hint Text"
-      floatingLabelText="Fixed Floating Label Text"
-      floatingLabelFixed={true}
     /><br />
     <TextField
-      hintText="Password Field"
-      floatingLabelText="Password"
-      type="password"
+      hintText="Hint Text"
     /><br />
     <TextField
-      hintText="MultiLine with rows: 2 and rowsMax: 4"
-      multiLine={true}
-      rows={2}
-      rowsMax={4}
+      hintText="Hint Text"
     /><br />
-    <TextField
-      hintText="Message Field"
-      floatingLabelText="MultiLine and FloatingLabel"
-      multiLine={true}
-      rows={2}
-    /><br />
-    <TextField
-      hintText="Full width"
-      fullWidth={true}
-    />
   </div>
   </MuiThemeProvider>
 )
