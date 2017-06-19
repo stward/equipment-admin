95568ec572064fd14540bdf3286265d0405ba560
Spencer Ward <spencertward@gmail.com>
2017-06-19 15:24:18 -0600

    Added additional fields to form

--- src/Edit.js
+++ src/Edit.js
@@ -50,108 +50,106 @@ class Edit extends Component {
 
     return (
       <div>
-      <div>
         <MuiThemeProvider muiTheme={getMuiTheme()}>
           <RaisedButton className={classes.button} label="Back" primary={false} href={'/'} />
         </MuiThemeProvider>
-      </div>
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <Paper className={classes.paper} zDepth={1}>
-        <h1>Edit / Create Details</h1>
-        <div className={classes.editContainer}>
-          <TextField
-            floatingLabelText="Equipment Name"
-          />
-          <TextField
-            floatingLabelText="LTL"
-          />
-          <TextField
-            floatingLabelText="Max Length"
-          />
-          <TextField
-            floatingLabelText="Max Width"
-          />
-          <TextField
-            floatingLabelText="Max Height"
-          />
-          <TextField
-            floatingLabelText="Max Weight"
-          />
-          <TextField
-            floatingLabelText="OD Length"
-          />
-          <TextField
-            floatingLabelText="OD Width"
-          />
-          <TextField
-            floatingLabelText="OD Height"
-          />
-          <TextField
-            floatingLabelText="OD Weight"
-          />
-          <TextField
-            floatingLabelText="OD Bump"
-          />
-          <TextField
-            floatingLabelText="Rate"
-          />
-        </div>
-        <div className={classes.editContainer}>
-          <TextField
-            floatingLabelText="Day Cost"
-          />
-          <TextField
-            floatingLabelText="Day Cost Max Distance"
-          />
-          <TextField
-            floatingLabelText="Escort Height"
-          />
-          <TextField
-            floatingLabelText="Escort Rate Bump"
-          />
-          <TextField
-            floatingLabelText="Escort Width"
-          />
-          <TextField
-            floatingLabelText="Escort Length"
-          />
-          <TextField
-            floatingLabelText="Int'l Border Cost Bump %"
-          />
-          <TextField
-            floatingLabelText="LTL Int'l Rate Bump %"
-          />
-          <TextField
-            floatingLabelText="LTL Max Length %"
-          />
-          <TextField
-            floatingLabelText="LTL Max Weight %"
-          />
-          <TextField
-            floatingLabelText="LTL Short Load Length Limit"
-          />
-          <TextField
-            floatingLabelText="LTL Rate Bump"
-          />
-          <TextField
-            floatingLabelText="OD US Permit Fee"
-          />
-          <TextField
-            floatingLabelText="OD Width"
-          />
-          <TextField
-            floatingLabelText="Overweight Interval"
-          />
-          <TextField
-            floatingLabelText="Overweight Rate Bump"
-          />
-          <TextField
-            floatingLabelText="Overweight Threshold"
-          />
-        </div>
-        <RaisedButton className={classes.button} label="Submit" primary={false} href={'/home'} />
-        </Paper>
-      </MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <Paper className={classes.paper} zDepth={1}>
+            <h1>Edit / Create Details</h1>
+            <div className={classes.editContainer}>
+              <TextField
+                floatingLabelText="Equipment Name"
+              />
+              <TextField
+                floatingLabelText="LTL"
+              />
+              <TextField
+                floatingLabelText="Max Length"
+              />
+              <TextField
+                floatingLabelText="Max Width"
+              />
+              <TextField
+                floatingLabelText="Max Height"
+              />
+              <TextField
+                floatingLabelText="Max Weight"
+              />
+              <TextField
+                floatingLabelText="OD Length"
+              />
+              <TextField
+                floatingLabelText="OD Width"
+              />
+              <TextField
+                floatingLabelText="OD Height"
+              />
+              <TextField
+                floatingLabelText="OD Weight"
+              />
+              <TextField
+                floatingLabelText="OD Bump"
+              />
+              <TextField
+                floatingLabelText="Rate"
+              />
+            </div>
+            <div className={classes.editContainer}>
+              <TextField
+                floatingLabelText="Day Cost"
+              />
+              <TextField
+                floatingLabelText="Day Cost Max Distance"
+              />
+              <TextField
+                floatingLabelText="Escort Height"
+              />
+              <TextField
+                floatingLabelText="Escort Rate Bump"
+              />
+              <TextField
+                floatingLabelText="Escort Width"
+              />
+              <TextField
+                floatingLabelText="Escort Length"
+              />
+              <TextField
+                floatingLabelText="Int'l Border Cost Bump %"
+              />
+              <TextField
+                floatingLabelText="LTL Int'l Rate Bump %"
+              />
+              <TextField
+                floatingLabelText="LTL Max Length %"
+              />
+              <TextField
+                floatingLabelText="LTL Max Weight %"
+              />
+              <TextField
+                floatingLabelText="LTL Short Load Length Limit"
+              />
+              <TextField
+                floatingLabelText="LTL Rate Bump"
+              />
+              <TextField
+                floatingLabelText="OD US Permit Fee"
+              />
+              <TextField
+                floatingLabelText="OD Width"
+              />
+              <TextField
+                floatingLabelText="Overweight Interval"
+              />
+              <TextField
+                floatingLabelText="Overweight Rate Bump"
+              />
+              <TextField
+                floatingLabelText="Overweight Threshold"
+              />
+            </div>
+            <RaisedButton className={classes.button} label="Submit" primary={false} href={'/'} />
+          </Paper>
+        </MuiThemeProvider>
       </div>
     )
   }
